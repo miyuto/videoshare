c2bb62a82da20cb5817a315a2607bc5d4ced2ede
pham son <miyuto@phamnoMacBook-Pro.local>
2020-12-29 11:54:45 +0900

    create shortcode add cat_tags

--- themes/videopro/functions.php
+++ themes/videopro/functions.php
@@ -1349,3 +1349,43 @@ function is_video_listing_template($page_name) {
         return false;
     }
 }
+
+
+// Bổ sung shortcodes hiển thị custom tags
+function videopro_cat_tags_shortcode($atts, $content){
+	$term = isset($atts['tax']) ? $atts['tax'] : 'post_tag';
+
+	$alphas = array_merge(array('0-9'), range('A', 'Z'));
+	$alphas = apply_filters('videopro_v_cats_characters', $alphas);
+	
+	$html = '';
+	$html .= apply_filters('videopro_v_cats_before_listing', '<ul class="cat-listing ' . $term . '-listing list_tag">', $term);
+	foreach($alphas as $alpha){
+		$cats = videopro_get_terms_by_first_letter($alpha, $term);
+		
+		if(count($cats) > 0){
+			/**
+			 * filter heading of listing
+			 */
+		//	$html .= apply_filters('videopro_v_cats_heading', '<h3 class="v-cats-heading h1">' . $alpha . '</h3>', $alpha);
+			/**
+			 * filter before listing
+			 */
+			foreach($cats as $cat){
+				$item = '<li><a href="' . get_term_link($cat) . '" title="' . $cat->name  . '">' . $cat->name . '</a></li>';
+				
+				/**
+				 * filter heading of listing item
+				 */
+				$html .= apply_filters('videopro_v_cats_item',$item, $term, $cat);
+			}
+			
+			/**
+			 * filter after listing
+			 */
+		}
+	}
+	$html .= apply_filters('videopro_v_cats_after_listing', '</ul>', $term);	
+	return $html;
+}
+add_shortcode('v_cat_tags','videopro_cat_tags_shortcode');
